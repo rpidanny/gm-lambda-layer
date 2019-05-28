1e721a7e5c9c00ece0ef14435c32bb06382f3cfc
rpidanny <abhishekmaharjan1993@gmail.com>
2019-05-28 16:03:21 +0200

    Add regions

--- publish.sh
+++ publish.sh
@@ -4,8 +4,25 @@ GM_VERSION=1.3.31
 LAYER_NAME='graphicsmagick'
 
 REGIONS='
+ap-northeast-1
+ap-northeast-2
+ap-east-1
+ap-south-1
+ap-southeast-1
+ap-southeast-2
+ca-central-1
+eu-north-1
+eu-central-1
 eu-west-1
+eu-west-2
+eu-west-3
+sa-east-1
+us-east-1
+us-east-2
+us-west-1
+us-west-2
 '
+
 PROFILE='dev'
 
 for region in $REGIONS; do
