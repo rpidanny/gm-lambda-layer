0a5d9c7caaa0111bfb27917b2d190c10adbeb452
rpidanny <abhishekmaharjan1993@gmail.com>
2019-06-27 15:12:28 +0200

    Fix version

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 export GM_VERSION=1.3.31
 
 docker build --build-arg GM_VERSION -t gm-lambda-layer .
-docker run --rm gm-lambda-layer cat /tmp/gm-${GIT_VERSION}.zip > ./layer.zip
+docker run --rm gm-lambda-layer cat /tmp/gm-${GM_VERSION}.zip > ./layer.zip
