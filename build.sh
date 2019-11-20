c993bf1a32121360002f4559e0c411194f8fef97
Richard Davison <richard.davison1@gmail.com>
2019-11-20 16:48:34 +0100

    Add support for AWS Linux 2

--- build.sh
+++ build.sh
@@ -5,3 +5,12 @@ export GS_VERSION=9.50
 
 docker build --build-arg GM_VERSION --build-arg GS_VERSION -t gm-lambda-layer .
 docker run --rm gm-lambda-layer cat /tmp/gm-${GM_VERSION}.zip > ./layer.zip
+
+FONT_TARGET=./tmp/share/fonts/default/
+mkdir -p $FONT_TARGET
+cp -R ./font/* $FONT_TARGET
+cd tmp && zip -ur ./../layer.zip ./share
+cd ..
+rm -rf ./tmp
+
+
